ff2c1eaca8891fdbb8b4bfc7e7d079f75c5ca315
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2022-07-09 10:57:38 -0500

    agregar filtro de excepciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
+import { APP_FILTER } from '@nestjs/core';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthModule } from './auth/auth.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { CoreModule } from './core/core.module';
+
+import { HttpExceptionFilter } from './core/exception-filters/http-exception-filter';
 
 @Module({
   imports: [
@@ -25,10 +29,15 @@ import { UsuariosModule } from './usuarios/usuarios.module';
       }),
       inject: [ConfigService],
     }),
+    CoreModule,
     AuthModule,
     UsuariosModule,
   ],
-  controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/core/exception-filters/http-exception-filter.ts
@@ -0,0 +1,26 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+
+    response.status(status).json({
+      message: exception.message,
+      error: exception.name,
+      //   details: error.errors,
+      status: status,
+      path: request.url,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
